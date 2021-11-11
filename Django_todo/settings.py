a5cd40c1f97d00798ed91d9b40d6efae462029c5
Emmett Becirovic <76811586+bozy15@users.noreply.github.com>
2021-11-11 15:11:15 +0000

    Add environment varibles

--- Django_todo/settings.py
+++ Django_todo/settings.py
@@ -94,7 +94,7 @@ if development:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+            "NAME": BASE_DIR / "db.sqlite3",
         }
     }
 else:
