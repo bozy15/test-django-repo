8b0b6fe856dd84280d6d408549c39d3dc265adb9
Emmett Becirovic <76811586+bozy15@users.noreply.github.com>
2021-11-11 15:39:17 +0000

    test

--- Django_todo/settings.py
+++ Django_todo/settings.py
@@ -29,8 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = (
-    os.environ.get("SECRET_KEY",
-    "django-insecure--d7tzn_r21)!jcy1ghm5zh&m3^7$8$k6xm_ls&uaa2$je$!@g*"),
+    os.environ.get(
+        "SECRET_KEY",
+        "django-insecure--d7tzn_r21)!jcy1ghm5zh&m3^7$8$k6xm_ls&uaa2$je$!@g*",
+    ),
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -41,7 +43,10 @@ DEBUG = development
 if development:
     ALLOWED_HOSTS = ["localhost"]
 else:
-    ALLOWED_HOSTS = [os.environ.get("HEROKU_HOSTNAME"), "test-django-repo.herokuapp.com"]
+    ALLOWED_HOSTS = [
+        os.environ.get("HEROKU_HOSTNAME"),
+        "test-django-repo.herokuapp.com",
+    ]
 
 
 # Application definition
@@ -99,7 +104,9 @@ if development:
     }
 else:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        "default": dj_database_url.parse(
+            "postgres://ssmuumsuvetzsw:3fce6492261b131c45e3eb55fd646ebcb672b9869318afb1a0cb59d2ee42e3ca@ec2-54-171-25-232.eu-west-1.compute.amazonaws.com:5432/d3nfigi9gn3obd"
+        )
     }
 
 
